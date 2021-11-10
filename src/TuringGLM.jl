0c4d9a96431c5b39a51de9d133b8312e32acb668
Jose Storopoli <thestoropoli@gmail.com>
2021-11-10 11:45:08 -0300

    removed unused comments

--- src/TuringGLM.jl
+++ src/TuringGLM.jl
@@ -11,8 +11,6 @@ using Reexport: @reexport
 @reexport begin
     using Turing
     using MixedModels: @formula
-    # TODO: review which Link types will be used
-    #   See GLM.jl/src/glmtools.jl and GLM.jl/src/GLM.jl
 end
 
 include("formula.jl")
