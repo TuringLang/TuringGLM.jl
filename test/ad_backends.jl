85e2b61a18998060f84df425de6098eac390b83a
Jose Storopoli <jose@storopoli.io>
2022-09-05 17:46:39 -0300

    added tests for all ADs

---
+++ test/ad_backends.jl
@@ -0,0 +1,34 @@
+@timed_testset "ad_backends" begin
+    DATA_DIR = joinpath("..", "data")
+    cheese = CSV.read(joinpath(DATA_DIR, "cheese.csv"), DataFrame)
+    f = @formula(y ~ (1 | cheese) + background)
+    m = turing_model(f, cheese)
+    # only running 2 samples to test if the different ADs runs
+    @timed_testset "ForwardDiff" begin
+        Turing.setadbackend(:forwarddiff)
+        chn = sample(seed!(123), m, NUTS(), MCMCThreads(), 10, 2)
+        @assert chn isa Chains
+    end
+    @timed_testset "Tracker" begin
+        using Tracker
+        Turing.setadbackend(:tracker)
+        chn = sample(seed!(123), m, NUTS(), MCMCThreads(), 10, 2)
+        @assert chn isa Chains
+    end
+    @timed_testset "Zygote" begin
+        using Zygote
+        Turing.setadbackend(:zygote)
+        chn = sample(seed!(123), m, NUTS(), MCMCThreads(), 10, 2)
+        @assert chn isa Chains
+    end
+    @timed_testset "ReverseDiff" begin
+        using ReverseDiff
+        Turing.setadbackend(:reversediff)
+        Turing.setrdcache(true)
+        chn = sample(seed!(123), m, NUTS(), MCMCThreads(), 10, 2)
+        @assert chn isa Chains
+    end
+    # go back to defaults
+    Turing.setrdcache(false)
+    Turing.setadbackend(:forwarddiff)
+end
