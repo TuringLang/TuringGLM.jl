73775d74fd9b96a0e5fb27031c2a4afe870cc897
Penelope Yong <penelopeysm@gmail.com>
2025-06-18 16:01:58 +0100

    bump versions, update AD tests, update workflows (#166)
    This fixes all the existing CompatHelper PRs
    
    Closes #160
    Closes #161
    Closes #163 
    Closes #164 
    
    And also updates the AD testing to use Mooncake (and removes the
    previously commented-out Tracker and Zygote tests).
    
    And also updates the test and format workflows to more closely match the
    rest of TuringLang. In particular:
    - uses
    [TuringLang/actions/Format](https://github.com/TuringLang/actions?tab=readme-ov-file#format)
    for workflow standardisation
    - tests on both Julia 1.10 and Julia 1.11
    - removes the `arch=x64` matrix variable which is no longer appropriate
    for macOS-latest (should be `aarch64`, but leaving it out is easier as
    it will allow GitHub to automatically determine the right architecture)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![Build Status](https://github.com/TuringLang/TuringGLM.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/TuringLang/TuringGLM.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/TuringLang/TuringGLM.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/TuringLang/TuringGLM.jl)
 [![Coverage](https://coveralls.io/repos/github/TuringLang/TuringGLM.jl/badge.svg?branch=main)](https://coveralls.io/github/TuringLang/TuringGLM.jl?branch=main)
-[![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
+[![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/JuliaDiff/BlueStyle)
 [![ColPrac: Contributor's Guide on Collaborative Practices for Community Packages](https://img.shields.io/badge/ColPrac-Contributor's%20Guide-blueviolet)](https://github.com/SciML/ColPrac)
 
 TuringGLM makes easy to specify Bayesian **G**eneralized **L**inear **M**odels using the formula syntax and returns an instantiated [Turing](https://github.com/TuringLang/Turing.jl) model.
--- test/ad_backends.jl
+++ test/ad_backends.jl
@@ -1,28 +1,20 @@
-@timed_testset "ad_backends" begin
+using ForwardDiff: ForwardDiff
+using ReverseDiff: ReverseDiff
+using Mooncake: Mooncake
+
+@timed_testset "ad_backends.jl" begin
     DATA_DIR = joinpath("..", "data")
     cheese = CSV.read(joinpath(DATA_DIR, "cheese.csv"), DataFrame)
     f = @formula(y ~ (1 | cheese) + background)
     m = turing_model(f, cheese)
-    # only running 2 samples to test if the different ADs runs
-    @timed_testset "ForwardDiff" begin
-        chn = sample(m, NUTS(; adtype=AutoForwardDiff(; chunksize=8)), 2)
-        @test chn isa Chains
-    end
-    # TODO: fix Tracker tests
-    # @timed_testset "Tracker" begin
-    #     using Tracker
-    #     chn = sample(m, NUTS(; adtype=AutoTracker()), 2)
-    #     @test chn isa Chains
-    # end
-    # TODO: fix Zygote tests
-    # @timed_testset "Zygote" begin
-    #     using Zygote
-    #     chn = sample(m, NUTS(; adtype=AutoZygote()), 2)
-    #     @test chn isa Chains
-    # end
-    @timed_testset "ReverseDiff" begin
-        using ReverseDiff
-        chn = sample(m, NUTS(; adtype=AutoReverseDiff(; compile=true)), 2)
-        @test chn isa Chains
+
+    ADTYPES = [
+        AutoForwardDiff(),
+        AutoReverseDiff(; compile=false),
+        AutoReverseDiff(; compile=true),
+        AutoMooncake(; config=nothing),
+    ]
+    @testset "$adtype" for adtype in ADTYPES
+        @test sample(m, NUTS(; adtype=adtype), 20) isa Chains
     end
 end
